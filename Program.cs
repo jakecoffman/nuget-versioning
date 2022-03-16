dba39831953a0b9a4082f9368681799b53903414
Jake Coffman <jake@jakecoffman.com>
2022-03-15 19:13:27 -0500

    more cases

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using NuGet.Versioning;
 
-// output: True
-Console.WriteLine(SemanticVersion.Parse("1.0.0") == SemanticVersion.Parse("1.0.0+1"));
+Console.WriteLine(SemanticVersion.Parse("1.0.0") == SemanticVersion.Parse("1.0.0+1")); // True
+Console.WriteLine(SemanticVersion.Parse("1.0.0-alpha") == SemanticVersion.Parse("1.0.0-ALPHA")); // True
+//SemanticVersion.Parse("1.0.0.1"); // ArgumentException
+//SemanticVersion.Parse("1") // ArgumentException
